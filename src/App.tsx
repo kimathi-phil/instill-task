5d73f1fc7233b3d0f39656353b2dad9d769210e4
Philip Kimathi <kimathi.phil@gmail.com>
2023-09-11 15:29:56 +0300

    feat: Paginate results

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,34 @@ import React from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 import MovieList from "./components/MovieList";
+import Pagination from "./components/Pagination";
 import useMovies from "./hooks/useMovies";
 
 const App: React.FC = () => {
-  const { movies, searchMovies } = useMovies();
+  const {
+    movies,
+    searchMovies,
+    currentPage,
+    setCurrentPage,
+    currentSearchTerm,
+  } = useMovies();
+  const totalPages = 100;
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    searchMovies(currentSearchTerm);
+  };
 
   return (
     <div className="App">
       <h1 className="text-3xl font-bold p-4">Movie Search</h1>
       <SearchBar onSearch={searchMovies} />
       <MovieList movies={movies} />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 };
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -9,7 +9,7 @@ interface Props {
 const MovieList: React.FC<Props> = ({ movies }) => {
   return (
     <div className="p-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {movies &&
           movies.map((movie) => <MovieItem key={movie.imdbID} movie={movie} />)}
       </div>
